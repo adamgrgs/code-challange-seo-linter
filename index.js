d730cdc926a00ef82387b71af6bcfeafac82d155
guitarbien <guitarbien@gmail.com>
2018-04-06 00:32:48 +0800

    可傳入 readStream 參數作為 input，或是由 config 設定的 html file path 執行解析

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports.exec = function() {
+module.exports.exec = function(readStream = null) {
   const fs = require('fs'),
     config = require('config'),
     basename = require('basename'),
@@ -28,14 +28,27 @@ module.exports.exec = function() {
   });
 
   const run = function (rules) {
-    JSDOM.fromFile(config.inputTarget).then(function (dom) {
-      rules.forEach((fn) => {
-        const result = fn(dom);
+    // read stream
+    if (readStream !== null) {
+      let dom = new JSDOM(readStream.read().toString());
+      lint(rules, dom);
 
-        if (result.hasError()) {
-          console.log(result.output);
-        }
-      });
+      return;
+    }
+
+    // read file
+    JSDOM.fromFile(config.inputTarget).then(function(dom) {
+      lint(rules, dom);
+    });
+  };
+
+  const lint = function (rules, dom) {
+    rules.forEach((fn) => {
+      const result = fn(dom);
+
+      if (result.hasError()) {
+        console.log(result.output);
+      }
     });
   };
 };
