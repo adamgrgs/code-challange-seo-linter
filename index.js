ad9d7087c5237167dd6bca5d8e8c04e5ccee2d08
guitarbien <guitarbien@gmail.com>
2018-04-06 01:03:37 +0800

    可輸出到檔案

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ module.exports.exec = function(readStream = null) {
   const fs = require('fs'),
     config = require('config'),
     basename = require('basename'),
-    {JSDOM} = require("jsdom");
+    {JSDOM} = require("jsdom"),
+    EOL = require('os').EOL;
 
   fs.readdir(config.rulesFolder, function (err, ruleList) {
-    if (err) {
-      throw err;
-    }
+    throwIfErr(err);
 
     run(
       ruleList.filter((filename) => {
@@ -21,11 +20,16 @@ module.exports.exec = function(readStream = null) {
 
         return config.runStartsWithOnly.find((prefix) => filename.startsWith(prefix));
       })
-        .map((filename) => {
-          return require(config.rulesFolder + '/' + basename(filename));
-        })
+      .map((filename) => {
+        return require(config.rulesFolder + '/' + basename(filename));
+      })
     );
   });
+
+  const throwIfErr = function(err) {
+    if (err)
+      throw err;
+  };
 
   const run = function (rules) {
     // read stream
@@ -46,9 +50,21 @@ module.exports.exec = function(readStream = null) {
     rules.forEach((fn) => {
       const result = fn(dom);
 
-      if (result.hasError()) {
-        console.log(result.output);
+      if (!result.hasError()) {
+        return;
       }
+
+      // output to file
+      if (config.outputTarget !== '') {
+        fs.appendFile(config.outputTarget, result.output + EOL, function (err) {
+          throwIfErr(err);
+        });
+
+        return;
+      }
+
+      // output to console
+      console.log(result.output);
     });
   };
 };
