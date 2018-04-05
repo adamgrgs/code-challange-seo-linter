4481c798cdfd0632bc46da41d6c58decb0ae8853
guitarbien <guitarbien@gmail.com>
2018-04-05 16:17:03 +0800

    使用 config 套件讀取設定

--- index.js
+++ index.js
@@ -1,35 +1,22 @@
-const fs = require('fs');
-const basename = require('basename');
-const {JSDOM} = require("jsdom");
+const fs = require('fs'),
+  config = require('config'),
+  basename = require('basename'),
+  {JSDOM} = require("jsdom");
 
-fs.readFile('config.json', (err, data) => {
-  scanFolder(JSON.parse(filterError(err, data)));
-});
-
-const filterError = function(err, data) {
+fs.readdir(config.rulesFolder, function (err, ruleList) {
   if (err) {
     throw err;
   }
 
-  return data;
-};
-
-const scanFolder = function(config) {
-  fs.readdir(config.rulesFolder, loadAllRules(config));
-};
-
-const loadAllRules = function (config) {
-  return function (err, ruleList) {
-    run(
-      filterError(err, ruleList).filter((filename) =>
-        filename.startsWith(config.rulesPrefix) && filename.endsWith(config.rulesSuffix)
-      )
-      .map((filename) => {
-        return require(config.rulesFolder + '/' + basename(filename));
-      })
-    );
-  };
-};
+  run(
+    ruleList.filter((filename) =>
+      filename.startsWith(config.rulesPrefix) && filename.endsWith(config.rulesSuffix)
+    )
+    .map((filename) => {
+      return require(config.rulesFolder + '/' + basename(filename));
+    })
+  );
+});
 
 const run = function (rules) {
   // todo free to chain any rules by user
