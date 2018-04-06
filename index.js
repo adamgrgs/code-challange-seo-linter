b14ef4e786a5611857834575868155084cb2bb16
guitarbien <guitarbien@gmail.com>
2018-04-07 00:53:22 +0800

    調整為 construct 注入 config

--- index.js
+++ index.js
@@ -1,52 +1,59 @@
-module.exports.exec = function(readStream = null) {
-  const fs = require('fs'),
-    config = require('config'),
-    basename = require('basename'),
-    {JSDOM} = require("jsdom"),
-    EOL = require('os').EOL;
+const fs = require('fs'),
+  basename = require('basename'),
+  {JSDOM} = require("jsdom"),
+  EOL = require('os').EOL;
 
-  fs.readdir(config.rulesFolder, function (err, ruleList) {
+// construct
+function SeoLinter(config) {
+  this.config = config;
+}
+
+SeoLinter.prototype.exec = function (readStream = null) {
+  this.readStream = readStream;
+  const self = this;
+
+  fs.readdir(self.config.rulesFolder, function (err, ruleList) {
     throwIfErr(err);
 
     run(
       ruleList.filter((filename) => {
-        if (filename.match(config.filenamePattern) === null) {
+        if (filename.match(self.config.filenamePattern) === null) {
           return false;
         }
 
-        if (config.runStartsWithOnly.length <= 0) {
+        if (self.config.runStartsWithOnly.length <= 0) {
           return true;
         }
 
-        return config.runStartsWithOnly.find((prefix) => filename.startsWith(prefix));
+        return self.config.runStartsWithOnly.find((prefix) => filename.startsWith(prefix));
       })
       .map((filename) => {
-        return require(config.rulesFolder + '/' + basename(filename));
+        return require(self.config.rulesFolder + '/' + basename(filename));
       })
     );
   });
 
-  const throwIfErr = function(err) {
+  function throwIfErr (err) {
     if (err)
       throw err;
-  };
+  }
 
-  const run = function (rules) {
+  function run(rules) {
     // read stream
-    if (readStream !== null) {
-      let dom = new JSDOM(readStream.read().toString());
+    if (self.readStream !== null) {
+      let dom = new JSDOM(self.readStream.read().toString());
       lint(rules, dom);
 
       return;
     }
 
     // read file
-    JSDOM.fromFile(config.inputTarget).then(function(dom) {
+    JSDOM.fromFile(self.config.inputTarget).then(function(dom) {
       lint(rules, dom);
     });
-  };
+  }
 
-  const lint = function (rules, dom) {
+  function lint (rules, dom) {
     rules.forEach((fn) => {
       const result = fn(dom);
 
@@ -55,8 +62,8 @@ module.exports.exec = function(readStream = null) {
       }
 
       // output to file
-      if (config.outputTarget !== '') {
-        fs.appendFile(config.outputTarget, result.output + EOL, function (err) {
+      if (self.config.outputTarget !== '') {
+        fs.appendFile(self.config.outputTarget, result.output + EOL, function (err) {
           throwIfErr(err);
         });
 
@@ -66,5 +73,7 @@ module.exports.exec = function(readStream = null) {
       // output to console
       console.log(result.output);
     });
-  };
+  }
 };
+
+module.exports = SeoLinter;
